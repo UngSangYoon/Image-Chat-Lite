e43fe01099a87efdbe616737bb5900cbc1b2cb44
UngSangYoon <124053569+UngSangYoon@users.noreply.github.com>
2024-11-23 12:23:12 +0900

    Add files via upload

---
+++ llava-ios/UI/Screens/InferenceScreen/CameraPicker.swift
@@ -0,0 +1,45 @@
+//
+//  CameraPicker.swift
+//  llava-ios
+//
+//  Created by 윤웅상 on 11/3/24.
+//
+import SwiftUI
+import UIKit
+
+struct CameraPicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraPicker
+
+        init(_ parent: CameraPicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
---
+++ llava-ios/llava_iosApp.swift
@@ -0,0 +1,13 @@
+//  llava_iosApp.swift
+//  llava-ios
+
+import SwiftUI
+
+@main
+struct llava_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
